c216d0320fd2d67f7348c18730e3f568bb018939
uzador <uzadorozhny@gmail.com>
2016-06-03 18:52:56 +0300

    Example with drawing chart

--- src/resources/static/js/myhighchartlib.js
+++ src/resources/static/js/myhighchartlib.js
@@ -4,14 +4,14 @@ function myhighchart() {
     });
 
     var secidCount = 0;
+    var marketNameCount = 0;
     $.getJSON('http://localhost:8080/get-secids', function (data) {
         secidCount = data.length
-    });
 
-    var marketNameCount = 0;
-        $.getJSON('http://localhost:8080/get-secids', function (data) {
+        $.getJSON('http://localhost:8080/get-marketname-by-secid?secid=' + secIdValue, function (marketNameData) {
             secidCount = data.length
         });
+    });
 
     $.ajaxSetup({
         async: true
@@ -132,3 +132,63 @@ function myhighchart() {
         });
     });*/
 }
+
+
+
+/*
+    function createChart() {
+
+        $('#container').highcharts('StockChart', {
+
+            rangeSelector: {
+                selected: 4
+            },
+
+            yAxis: {
+                labels: {
+                    formatter: function () {
+                        return (this.value > 0 ? ' + ' : '') + this.value + '%';
+                    }
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 2,
+                    color: 'silver'
+                }]
+            },
+
+            plotOptions: {
+                series: {
+                    compare: 'percent'
+                }
+            },
+
+            tooltip: {
+                pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+                valueDecimals: 2
+            },
+
+            series: seriesOptions
+        });
+    }
+
+    $.each(names, function (i, name) {
+
+        $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=' + name.toLowerCase() + '-c.json&callback=?',    function (data) {
+
+            seriesOptions[i] = {
+                name: name,
+                data: data
+            };
+
+            // As we're loading the data asynchronously, we don't know what order it will arrive. So
+            // we keep a counter and create the chart when all the data is loaded.
+            seriesCounter += 1;
+
+            if (seriesCounter === names.length) {
+                createChart();
+            }
+        });
+    });
+});
+*/
